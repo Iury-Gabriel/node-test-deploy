a3ccf817af63dcc79089b5f916558883328df36f
Iury Gabriel <157987319+Iury-Gabriel@users.noreply.github.com>
2024-11-02 17:18:51 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import express from "express";
+const express = require("express");
 
 const app = express();
 
@@ -6,8 +6,8 @@ app.get("/", (req, res) => {
     res.json({
         message: "Hello World"
     });
-})
+});
 
-app.listen({ port: 3000, host: '0.0.0.0' }, () => {
+app.listen(3000, '0.0.0.0', () => {
     console.log("Server started on port 3000");
-})
+});
